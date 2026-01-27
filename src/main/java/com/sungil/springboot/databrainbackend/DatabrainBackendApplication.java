2262cfe64602e80ed8c0210c0221eb2ac26f60c2
Lee-sung-il <wj132612@naver.com>
2026-01-27 21:34:18 +0900

    first commit

---
+++ src/main/java/com/sungil/springboot/databrainbackend/DatabrainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sungil.springboot.databrainbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabrainBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabrainBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sungil/springboot/databrainbackend/DatabrainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungil.springboot.databrainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabrainBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
