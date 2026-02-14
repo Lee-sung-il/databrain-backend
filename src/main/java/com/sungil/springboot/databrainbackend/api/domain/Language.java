360cf55fdd6afa412480c4bdda537a36af9d34c7
Lee-sung-il <wj132612@naver.com>
2026-02-14 14:44:21 +0900

    언어 처리

--- src/main/java/com/sungil/springboot/databrainbackend/api/domain/Language.java
+++ src/main/java/com/sungil/springboot/databrainbackend/api/domain/Language.java
@@ -1,5 +1,17 @@
 package com.sungil.springboot.databrainbackend.api.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.stream.Stream;
+
 public enum Language {
-    EN,KO,JA,ZH
+    EN,KO,JA,ZH;
+
+    @JsonCreator
+    public static Language from(String value) {
+        return Stream.of(Language.values())
+                .filter(lang -> lang.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElse(EN); // 매칭되는 게 없으면 기본값으로 EN 설정
+    }
 }
